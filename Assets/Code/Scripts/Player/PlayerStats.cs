54e3e48570cd43fcd92599e696ae4dc7057dc8f9
Victor Carrillo <a22viccarnav@inspedralbes.cat>
2026-06-23 12:30:20 +0200

    Add Player Movement

--- Assets/Code/Scripts/Player/PlayerStats.cs
+++ Assets/Code/Scripts/Player/PlayerStats.cs
@@ -5,5 +5,6 @@ public class PlayerStats: MonoBehaviour
     public int health;
     public float speed;
     public float dashSpeed;
-    public float jumpImpulse;
+    public float dashDuration;
+    public float dashCooldown;
 }
